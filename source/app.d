f459cba8153590d1ef524fbbf79b80353161a276
therealbluepandabear <justaguyoney@gmail.com>
2022-11-12 14:54:59 +1300

    Color customization.

--- source/app.d
+++ source/app.d
@@ -5,6 +5,18 @@ import core.thread.osthread;
 import core.time;
 import std.random;
 
+struct Color {
+	int r;
+	int g;
+	int b;
+}
+
+class Colors {
+	static Color Red = Color(255, 0, 0);
+	static Color Green = Color(0, 255, 0);
+	static Color Blue = Color(0, 0, 255);
+}
+
 struct Coordinates {
 	int x;
 	int y;
@@ -13,6 +25,7 @@ struct Coordinates {
 struct Cell {
 	Coordinates coordinates;
 	wchar content;
+	Color color;
 }
 
 struct Rect {
@@ -65,6 +78,7 @@ struct Rect {
 struct Label {
 	string text;
 	Coordinates origin;
+	Color color;
 
 	Cell[] toCells() {
 		Cell[] cells;
@@ -72,7 +86,7 @@ struct Label {
 		int loopCount = 0;
 
 		for (int x = origin.x; x < origin.x + text.length; ++x) {
-			cells ~= Cell(Coordinates(x, origin.y), text[loopCount]);
+			cells ~= Cell(Coordinates(x, origin.y), text[loopCount], color);
 			++loopCount;
 		}
 
@@ -111,6 +125,8 @@ class Canvas {
 
 				foreach (cell; this.cache) {
 					if (cell.coordinates.x == x && cell.coordinates.y == y && !(cellsDrawn.canFind!(cell => cell.coordinates.x == x && cell.coordinates.y == y))) {
+						writef("\x1b[38;2;%s;%s;%sm", cell.color.r, cell.color.g, cell.color.b);
+
 						write(cell.content);
 						cellsDrawn ~= cell;
 						cellFound = true;
@@ -133,6 +149,7 @@ void main() {
 	canvas.height = 10;
 
 	canvas
-		.cacheLabel(Label("Hi", Coordinates(1, 3)))
-		.draw();
+	.cacheLabel(Label("Hi", Coordinates(1, 3), Colors.Red))
+	.cacheLabel(Label("Hi", Coordinates(2, 4), Colors.Blue))
+	.draw();
 }
