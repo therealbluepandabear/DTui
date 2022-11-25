426b3395c1a76100f35f91ed503e34c193271ba3
therealbluepandabear <justaguyoney@gmail.com>
2022-11-25 20:01:05 +1300

    Shortened code a bit.

--- source/renderable/label.d
+++ source/renderable/label.d
@@ -24,23 +24,21 @@ class Label : Renderable {
 
         foreach (pos, character; text) {
             long position;
-            long index;
 
             if (textAlignment == TextAlignment.right) {
-                position = dimensions.width - pos - 1;
-                index = text.length - pos - 1;
-            } else {
+                position = dimensions.width - text.length + pos;
+            } else if (textAlignment == TextAlignment.left) {
                 position = pos;
-                index = pos;
+            } else {
             }
 
-            cells ~= Cell(Coordinates(cast(int)position, 0), text[index], color);
+            cells ~= Cell(Coordinates(cast(int)position, 0), text[pos], color);
 
         }
 
         for (int x = 0; x < dimensions.width; ++x) {
             for (int y = 0; y < dimensions.height; ++y) {
-                cells ~= Cell(Coordinates(x, y), ' ', color);
+                cells ~= Cell(Coordinates(x, y), '*', color);
             }
         }
 
