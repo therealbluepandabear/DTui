f4cd8bf61b3c0cfcf5deb441ec8163792eb64c5d
therealbluepandabear <justaguyoney@gmail.com>
2022-12-18 11:51:14 +1300

    Add constructor to `Vector` which takes one parameter called `size`. This will construct a vector with `x` and `y` values of `size`. This can be efficient in scenarios in which the user would want to define a `Vector` with an equal `x` and `y`.

--- source/vector.d
+++ source/vector.d
@@ -6,6 +6,11 @@ struct Vector {
     int x;
     int y;
 
+    this(int size) {
+        this.x = size;
+        this.y = size;
+    }
+
     string toString() const {
         return format("(%s, %s)", x, y);
     }
